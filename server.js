9a7dde8c0fbcd2d45b051ecac0951e75efb994f6
naeemahsan <30862057+naeemahsan@users.noreply.github.com>
2017-08-09 20:58:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',funtion(req,res){
+   res.send('ARticle one requested and will be served here') ;
+});
+app.get('/article-two',funtion(req,res){
+   res.send('ARticle two requested and will be served here') ;
+});
+app.get('/article-three',funtion(req,res){
+   res.send('ARticle three requested and will be served here') ;
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
