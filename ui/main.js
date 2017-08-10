f6fa6b17df37a1d54400271f474a28394e1fe4f3
naeemahsan <30862057+naeemahsan@users.noreply.github.com>
2017-08-10 13:32:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,19 @@
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function (){
+    var request = new XMHLTttpRequest();
     
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function(){
+      if (request.readState === XMLHttpRequest.DONE)  {
+        if (request.status ===200)  {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+      }
+    };
+    
+   request.open('GET', 'http://bitsupdates.imad.hasura-app.io/counter',true);
+   request.send(null);
 };
