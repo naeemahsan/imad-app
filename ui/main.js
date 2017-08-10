f069871ab62ab6a5534ea366f14e933c5c5f0800
naeemahsan <30862057+naeemahsan@users.noreply.github.com>
2017-08-10 11:35:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,12 @@ element.innerHTML = 'New Value';
 
 //move the image
 var img= document.getElementById('nodi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft +10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function (){
-    img.style.marginleft= '100px';
+    //img.style.marginleft= '100px';
+    var interval = setInterval(moveRight, 100);
 };
