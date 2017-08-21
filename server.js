7bf3ed5134d11c72cb9e1fb1ede06ec8c260f135
naeemahsan <30862057+naeemahsan@users.noreply.github.com>
2017-08-21 12:44:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    
+    user:'bitsupdates',
+    database:'bitsupdates',
+    host :'db.imad.hasura.app.io',
+    port : '5432',
+    password : 'db-bitsupdates-58569',  // user environment variables to keep the password hiddne process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -83,7 +93,7 @@ function createTemplate(data){
                     </h3>
                     <div>
                         ${date}
-                    </div>
+                    </div>0++
                     <div>
                        ${content}
                     </div>
@@ -97,6 +107,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test', function(err, result){
+            if (err){
+                    res.status(500).send(err.toString());
+            }else {
+                    res.send(JSON.stringify(result.rows));
+            }
+    });
+});
 var counter = 0;
 app.get('/counter',function(req,res){
     counter = counter +1;
