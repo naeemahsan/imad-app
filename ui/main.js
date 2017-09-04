17fe8afc92acc2dab767d0844fe707def1dd44e6
naeemahsan <30862057+naeemahsan@users.noreply.github.com>
2017-09-04 16:14:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,32 @@
-//counter code
-var button = document.getElementById('counter');
-
-button.onclick = function (){
-    var request = new XMHLTttpRequest();
-    
-    request.onreadystatechange = function(){
-      if (request.readState === XMLHttpRequest.DONE)  {
-        if (request.status ===200)  {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-      }
-    };
-    
-   request.open('GET', 'http://bitsupdates.imad.hasura-app.io/counter',true);
-   request.send(null);
-};
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+//submit username/password to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    var names = ['name1', 'name2','name3','name4'];
-    var list='';
-    for(var i=0; i<names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    // create request object
+    var request = new XMLHttpRequest();
+    
+    // capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            // take some action
+            if (request.status === 200) {
+                alert('Logged in successfully');
+            }else if (request.status === 403){
+                alert('Username/password is incorrect');
+            }else if (request.status === 500){
+                alert('Something went wrong on the server');
+            }
+        }
+    };
+        
+  
+    
+    // Make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://bitsupdates.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+    
 };
