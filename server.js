6fa37682e8558edaec4fe782a72e215853478f26
naeemahsan <30862057+naeemahsan@users.noreply.github.com>
2017-09-04 12:27:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     
@@ -15,6 +16,9 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.user(bodyParser.json()); // express framework is informed that when it see JSON type it should go to req.body
+
+
 
 var articles = {
     'article-one': {
@@ -117,6 +121,23 @@ function hash(input,salt){
 app.get('/hash/:input',function(req,res){
     var hashedString = hash(req.params.input,'this-is-some-random-string');
     res.send(hashedString);
+});
+
+
+//instead of aap.get we use app.post to keep the password secure and not logged in browser histroy
+app.post('/create-user',function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1,$2)', [username, dbString], function(err, result){
+       if (err){
+                    res.status(500).send(err.toString());
+            }else {
+                    res.send('User successfully created : ' + username));
+            }
+   });
 });
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
